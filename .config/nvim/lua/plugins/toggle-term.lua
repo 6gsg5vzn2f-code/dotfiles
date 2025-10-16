979f2745315e54d483d9f34a27cb23bccf7cfca6
6gsg5vzn2f-code <helices-barges-8b@icloud.com>
2025-10-16 11:43:28 +0800

    feat(toggleterm): <c-`> open float termial

--- .config/nvim/lua/plugins/toggle-term.lua
+++ .config/nvim/lua/plugins/toggle-term.lua
@@ -1,8 +1,17 @@
 -- https://github.com/akinsho/toggleterm.nvim
+-- https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
 -- <C-\> to open terminal
 return {
   -- amongst your other plugins
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  -- { "akinsho/toggleterm.nvim", version = "*", config = true },
   -- -- or
-  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      open_mapping = [[<c-`>]],
+      start_in_insert = true,
+      direction = "float", --vertical | float | tab
+    },
+  },
 }

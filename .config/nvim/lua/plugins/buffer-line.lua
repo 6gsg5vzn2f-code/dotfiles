afd32e87c4334e18f94f05e7187ff09082ca452d
6gsg5vzn2f-code <helices-barges-8b@icloud.com>
2025-10-24 14:08:51 +0800

    refactor(buffer-line): 移除tab的关闭图标，其余默认

--- .config/nvim/lua/plugins/buffer-line.lua
+++ .config/nvim/lua/plugins/buffer-line.lua
@@ -4,31 +4,9 @@ return {
     -- 合并默认配置
     opts = vim.tbl_deep_extend("force", opts, {
       options = {
-        indicator = {
-          -- icon = "❯",
-          style = "none", -- underline | icon
-        },
-        separator_style = { "", "" }, -- important "slant",
-        -- separator_style = "slant",
-        always_show_bufferline = true,
+        show_buffer_close_icons = false,
       },
     })
-    -- 自定义高亮
-    opts.highlights = {
-      buffer_selected = {
-        fg = "#C8D3F5",
-        bg = "NONE",
-        bold = true,
-        underline = false,
-        sp = vim.g.primary,
-      },
-      modified_selected = {
-        -- fg = vim.g.primary,
-        fg = "#EA8A63",
-        bg = "NONE",
-      },
-    }
-
     return opts
   end,
 }
